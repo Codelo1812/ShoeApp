c5f4be53669bab551e2aedc1400bec58659258c3
Nguyen <trinhtriennguyen@gmail.com>
2025-03-17 01:11:41 +0700

    intial commit

--- app/src/main/java/com/example/nhom10/ProfileActivity.java
+++ app/src/main/java/com/example/nhom10/ProfileActivity.java
@@ -1,6 +1,9 @@
 package com.example.nhom10;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +12,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    Button btnSetting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
+        btnSetting.findViewById(R.id.btnSetting);
+        btnSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(ProfileActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
